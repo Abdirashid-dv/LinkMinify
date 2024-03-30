b68585902307a1f49dbaddb5f01079a18e61c2ac
Abdurashid <Cabsho123@gmail.com>
2024-03-30 10:30:13 +0300

    Updated to vercel configuration file

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use((req, res, next) => {
 app.get("/*", linkController.redirectUrl);
 app.use("/api/v1", linkRoute);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
